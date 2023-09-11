149625bba3c8b226e3bb9565cbb3d2e4ef39ebe5
GeraRybak <144498120+GeraRybak@users.noreply.github.com>
2023-09-11 15:04:38 -0300

    Update Autorespuesta2.py

--- Autorespuesta2.py
+++ Autorespuesta2.py
@@ -1,9 +1,12 @@
 import time
 from telethon import TelegramClient, events
 
-# Ahi van los datos de la pagina https://my.telegram.org/apps
+# Ahi van los datos de la página https://my.telegram.org/apps
 api_id = 123456
 api_hash = '123445ansdjasdgja'
+
+# Tu ID de usuario de Telegram
+mi_id = 12345678  # Reemplaza con tu propio ID
 
 # Crear el cliente de Telegram
 client = TelegramClient('user', api_id, api_hash)
@@ -11,24 +14,28 @@ client = TelegramClient('user', api_id, api_hash)
 # Un diccionario para mantener el registro de la última respuesta para cada usuario
 last_response_time = {}
 
+# Tiempo de espera en segundos antes de poder volver a responder al mismo contacto
+tiempo_espera = 900  # 900 segundos = 15 minutos
+
 @client.on(events.NewMessage)
 async def handler(event):
     message = event.message
     sender = await message.get_sender()
 
-    # Verificar si el mensaje proviene de un usuario (no de un grupo o canal)
-    user_id = sender.id
+    # Verificar si el mensaje proviene de un usuario (no de un grupo o canal) y si el remitente es un contacto
+    if sender.id != mi_id and sender.is_contact:
+        user_id = sender.id
 
-    # Obtener la hora actual
-    current_time = time.time()
+        # Obtener la hora actual
+        current_time = time.time()
 
-    # Verificar si ya hemos respondido en los últimos 15 minutos (900 segundos)
-    if user_id not in last_response_time or current_time - last_response_time[user_id] >= 900:
-        # Responder al mensaje
-        await message.reply("Hola, buenos días. ¿En qué podemos ayudarte? Favor brindarnos el número de teléfono y los últimos 4 dígitos de la serie de la ONT.")
-        
-        # Registrar la hora de la última respuesta
-        last_response_time[user_id] = current_time
+        # Verificar si ya hemos respondido en los últimos 'tiempo_espera' segundos
+        if user_id not in last_response_time or current_time - last_response_time[user_id] >= tiempo_espera:
+            # Responder al mensaje
+            await message.reply("Hola, buenos días. ¿En qué podemos ayudarte? Favor brindarnos el número de teléfono y los últimos 4 dígitos de la serie de la ONT.")
+            
+            # Registrar la hora de la última respuesta
+            last_response_time[user_id] = current_time
 
 # Iniciar el cliente
 with client:
